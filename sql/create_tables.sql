dc421b14280edb962e6c7e07fdd00753e1ed40b2
Aivanus <ivank1995@hotmail.com>
2016-05-29 22:45:23 +0300

    taulujen luontilauseet lisätty

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,25 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Reader(
+	id SERIAL PPRIMARY KEY,
+	username varchar(50) NOT NULL UNIQUE,
+	password varchar(50) NOT NULL
+);
+
+CREATE TABLE Book(
+	id SERIAL PPRIMARY KEY,
+	title varchar(255) NOT NULL,
+	author varchar(255) NOT NULL
+);
+
+CREATE TABLE Review(
+	id SERIAL PPRIMARY KEY,
+	reader_id INTEGER REFERENCES Reader(id),
+	book_id INTEGER REFERENCES Book(id),
+	score INTEGER NOT NULL,
+	review_text varchar(max)
+);
+
+CREATE TABLE ReadingList(
+	id SERIAL PPRIMARY KEY,
+	reader_id INTEGER REFERENCES Reader(id),
+	book_id INTEGER REFERENCES Book(id)
+);
