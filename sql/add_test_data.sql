2aa45db1f83d3297544751713bfdc968f5a98cbb
Aivanus <ivank1995@hotmail.com>
2016-06-02 16:14:30 +0300

    taulujen poistolauseet ja testidata päivitetty

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -5,9 +5,9 @@ INSERT INTO Reader (username, password) VALUES ('DDO', '1337');
 -- Book-taulun testidata
 INSERT INTO Book (title, author) VALUES ('To Kill A Mockingbird', 'Harper Lee');
 INSERT INTO Book (title, author) VALUES ('Neuromancer', 'William Gibson');
--- Readinglist-taulun testidata
-INSERT INTO ReadingList (reader_id, book_id) VALUES (1, 1);
-INSERT INTO ReadingList (reader_id, book_id) VALUES (2, 2);
-INSERT INTO ReadingList (reader_id, book_id) VALUES (2, 1);
+-- MyBook-taulun testidata
+INSERT INTO MyBook (reader_id, book_id, status, added) VALUES (1, 1, 0, '2011-11-11');
+INSERT INTO MyBook (reader_id, book_id, status, added) VALUES (2, 2, 1, '2011-11-11');
+INSERT INTO MyBook (reader_id, book_id, status, added) VALUES (2, 1, 0, '2011-11-11');
 -- Review-taulun testidata
 INSERT INTO Review (reader_id, book_id, score, review_text) VALUES (1, 1, 5, 'Awesome book!');
