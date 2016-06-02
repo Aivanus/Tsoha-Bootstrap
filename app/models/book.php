e3cadb28c36ec5a116352757c628c5caee277f65
Aivanus <ivank1995@hotmail.com>
2016-06-02 15:19:19 +0300

    kirjan malli luotu

---
+++ app/models/book.php
@@ -0,0 +1,47 @@
+<?php
+
+class Book extends BaseModel{
+
+	public $id, $title, $author;
+
+	public function __construct($attributes){
+		parent::__construct($attributes);
+	}
+
+	public static function all(){
+		$query = DB::connection()->prepare('SELECT * FROM Book')
+		$query->execute();
+		$rows = $query->fetchAll();
+		$books = array();
+
+		foreach ($rows as $row) {
+			$books[] = new Book(array(
+				'id' => $row['id'],
+				'title' => $row['title'],
+				'author' => $row['author']
+			));		
+		}
+
+		return $games;
+	}
+
+	public static function find($id){
+		$query = DB::connection()->prepare(
+			'SELECT * FROM Book WHERE id = :id LIMIT 1'
+			);
+		$query->execute(array('id' => $id));
+		$row = $query->fetch();
+
+		if($row){
+			$book = new Book(array(
+				'id' => $row['id'],
+				'title' => $row['title'],
+				'author' => $row['author']
+				));
+
+			return $book;
+		}
+
+		return null;
+	}
+}
