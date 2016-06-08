c0ecc6fbb30ce9c3618453d4ebba94b43424f985
Aivanus <ivank1995@hotmail.com>
2016-06-08 20:30:22 +0300

    get_user_logged_in toteutettu

--- app/models/user.php
+++ app/models/user.php
@@ -1,10 +1,31 @@
 <?php
 
 class User extends BaseModel{
+
 	public $id, $username, $password;
 
 	public function __construct($attributes){
 		parent::__construct($attributes);
+	}
+
+	public static function find($id){
+		$query = DB::connection()->prepare(
+			'SELECT * FROM Reader WHERE id = :id LIMIT 1'
+			);
+		$query->execute(array('id' => $id));
+		$row = $query->fetch();
+
+		if($row){
+			$user = new User(array(
+				'id' => $row['id'],
+				'username' => $row['username'],
+				'password' => $row['password']
+				));
+
+			return $user;
+		}
+
+		return null;
 	}
 
 	public static function authenticate($username, $password){
