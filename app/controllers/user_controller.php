c7b1bfad2e9e78fdfa410dc279d5375e4dd0cdd8
Aivanus <ivank1995@hotmail.com>
2016-06-22 20:37:26 +0300

    Eri toimintojen uudelleenohjausta muokattu

--- app/controllers/user_controller.php
+++ app/controllers/user_controller.php
@@ -42,7 +42,7 @@ class UserController extends BaseController{
 	    }else{
 	      $_SESSION['user'] = $user->id;
 
-	      Redirect::to('/', array('message' => 'Welcome back ' . $user->username . '!'));
+	      Redirect::to('/user_page', array('message' => 'Welcome back ' . $user->username . '!'));
     	}
     }
 
